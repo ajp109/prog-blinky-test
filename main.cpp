d62caa7d881565a88db0cc2f30c01beff5c7e71b
ajp109 <57534418+ajp109@users.noreply.github.com>
2022-08-11 10:11:06 +0100

    Correct code in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,19 @@
 #include "mbed.h"
 
-int main() {
+int main()
+{
+    // Initialise the digital pin LED1 as an output
+    DigitalOut led(LED1);
+
+    // Loop forever...
     while (true) {
-        sleep();
+        // Switch the LED on
+        led = true;
+        // Wait for 200ms
+        thread_sleep_for(200);
+        // Switch the LED off
+        led = false;
+        // Wait for 300ms
+        thread_sleep_for(300);
     }
 }
